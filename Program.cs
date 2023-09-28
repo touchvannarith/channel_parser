4fbac3e2a0c35bfac9ea96e84038118ab7c0d414
touchvannarith <50974759+touchvannarith@users.noreply.github.com>
2023-09-28 22:59:26 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ChannelParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input channel:");
+            var title = Console.ReadLine();
+
+            // A list of valid notification channels
+            List<string> channels = new List<string>() { "BE", "FE", "QA", "Urgent" };
+
+            // A regular expression to match tags enclosed in square brackets
+            Regex regex = new Regex(@"\[(.*?)\]");
+
+            // A list to store the matched tags
+            List<string> tags = new List<string>();
+
+            // Loop through the matches and add them to the list
+            foreach (Match match in regex.Matches(title))
+            {
+                tags.Add(match.Groups[1].Value);
+            }
+
+            // Print the receive channels by filtering the tags
+            Console.WriteLine("Receive channels: " + string.Join(", ", tags.FindAll(tag => channels.Contains(tag))));
+        }
+    }
+}
